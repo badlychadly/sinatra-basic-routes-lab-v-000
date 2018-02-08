25c075a8537ec87bfd25c7c016baebb7829466f3
Chad Montoya <chadmontoya21@yahoo.com>
2018-02-08 21:37:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,12 +2,17 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
+  get '/' do
     "Homepage"
   end
 
   get '/name' do
     "My name is Maurice"
+  end
+
+
+  get '/hometown' do
+    "My hometown is Theed"
   end
 
 end
